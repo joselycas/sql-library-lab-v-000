35d16ba1d960731ed5d779d53337a3cc2f9dd44e
Jos Castro <joselycas@gmail.com>
2018-08-09 22:58:19 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -17,7 +17,7 @@ def select_value_and_count_of_most_prolific_species
   "SELECT characters.species, COUNT(*)
   FROM characters
   GROUP BY characters.species
-  ORDER BY COUNT(*) ASC"
+  ORDER BY COUNT(*)"
 end
 
 def select_name_and_series_subgenres_of_authors
