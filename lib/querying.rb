0978e7aaf0fec5baae0bbec101ee578e150c6e4b
Jos Castro <joselycas@gmail.com>
2018-08-09 23:34:49 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -37,6 +37,8 @@ end
 
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name
-  FROM character_books
+  FROM characters
+  JOIN books
+  ON books.series_id = characters.series_id
   "
 end
