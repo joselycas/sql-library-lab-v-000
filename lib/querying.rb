0200a26f6e30cf51ba808ea367056c82eea8bdef
Jos Castro <joselycas@gmail.com>
2018-08-09 23:29:35 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -36,7 +36,7 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT (*)characters.name
-  FROM characters
-  JOIN "
+  "SELECT characters.name
+  FROM characters_book
+  JOIN books ON  "
 end
