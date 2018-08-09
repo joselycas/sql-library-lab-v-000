adb6077e0985bc0774c3c213fa2a72516f36fbc5
Jos Castro <joselycas@gmail.com>
2018-08-09 22:51:21 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ end
 def select_value_and_count_of_most_prolific_species
   "SELECT (characters.species)
   FROM characters
-  GROUP BY characters.species LIMIT 1"
+  ORDER BY characters.species LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
