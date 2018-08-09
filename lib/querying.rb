6a0992e8d06faee8c059788175622fea72846d5c
Jos Castro <joselycas@gmail.com>
2018-08-09 23:39:08 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -36,7 +36,7 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, SUM(books)
+  "SELECT characters.name, SUM(books.id)
   FROM characters
   JOIN books
   ON books.series_id = characters.series_id
