ad6c577bdbce8a3dc6b4cbc1a0551555ac070810
Jos Castro <joselycas@gmail.com>
2018-08-09 22:41:58 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -9,7 +9,7 @@ end
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT characters.name, characters.motto
   FROM characters
-  ORDER BY characters.motto MIN 1 "
+  ORDER BY characters.motto "
 end
 
 
